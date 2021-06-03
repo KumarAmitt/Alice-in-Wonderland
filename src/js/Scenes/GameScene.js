a4f2c539d5a40e8369d1a4990e685ded8ace9d8c
KumarAmitt <amitt2015@outlook.com>
2021-06-03 11:58:10 +0530

    Revert to template version

--- src/js/Scenes/GameScene.js
+++ src/js/Scenes/GameScene.js
@@ -1,5 +1,5 @@
 import Phaser from 'phaser';
-// import logo from '../../assets/logo.png';
+import logo from '../../assets/logo.png';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -8,10 +8,10 @@ export default class GameScene extends Phaser.Scene {
 
   preload() {
     // load images
-    // this.load.image('logo', logo);
+    this.load.image('logo', logo);
   }
 
   create() {
-    // this.add.image(400, 300, 'logo');
+    this.add.image(400, 300, 'logo');
   }
 }
--- src/js/Scenes/PreloaderScene.js
+++ src/js/Scenes/PreloaderScene.js
@@ -16,90 +16,90 @@ export default class PreloaderScene extends Phaser.Scene {
   }
 
   preload() {
+    // add logo image
+    this.add.image(400, 200, 'logo');
 
-    // this.add.image(400, 200, 'logo');
+    // display progress bar
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
 
+    const { width } = this.cameras.main;
+    const { height } = this.cameras.main;
+    const loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff',
+      },
+    });
+    loadingText.setOrigin(0.5, 0.5);
 
-    // const progressBar = this.add.graphics();
-    // const progressBox = this.add.graphics();
-    // progressBox.fillStyle(0x222222, 0.8);
-    // progressBox.fillRect(240, 270, 320, 50);
+    const percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff',
+      },
+    });
+    percentText.setOrigin(0.5, 0.5);
 
-    // const { width } = this.cameras.main;
-    // const { height } = this.cameras.main;
-    // const loadingText = this.make.text({
-    //   x: width / 2,
-    //   y: height / 2 - 50,
-    //   text: 'Loading...',
-    //   style: {
-    //     font: '20px monospace',
-    //     fill: '#ffffff',
-    //   },
-    // });
-    // loadingText.setOrigin(0.5, 0.5);
+    const assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 50,
+      text: '',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff',
+      },
+    });
+    assetText.setOrigin(0.5, 0.5);
 
-    // const percentText = this.make.text({
-    //   x: width / 2,
-    //   y: height / 2 - 5,
-    //   text: '0%',
-    //   style: {
-    //     font: '18px monospace',
-    //     fill: '#ffffff',
-    //   },
-    // });
-    // percentText.setOrigin(0.5, 0.5);
+    // update progress bar
+    this.load.on('progress', (value) => {
+      percentText.setText(`${parseInt(value * 100, 10)}%`);
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(250, 280, 300 * value, 30);
+    });
 
-    // const assetText = this.make.text({
-    //   x: width / 2,
-    //   y: height / 2 + 50,
-    //   text: '',
-    //   style: {
-    //     font: '18px monospace',
-    //     fill: '#ffffff',
-    //   },
-    // });
-    // assetText.setOrigin(0.5, 0.5);
+    // update file progress text
+    this.load.on('fileprogress', (file) => {
+      assetText.setText(`Loading asset: ${file.key}`);
+    });
 
+    // remove progress bar when complete
+    this.load.on('complete', () => {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+      this.ready();
+    });
 
-    // this.load.on('progress', (value) => {
-    //   percentText.setText(`${parseInt(value * 100, 10)}%`);
-    //   progressBar.clear();
-    //   progressBar.fillStyle(0xffffff, 1);
-    //   progressBar.fillRect(250, 280, 300 * value, 30);
-    // });
+    this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
 
+    // load assets needed in our game
+    this.load.image('blueButton1', blueButton1);
+    this.load.image('blueButton2', blueButton2);
+    this.load.image('phaserLogo', phaserLogo);
 
-    // this.load.on('fileprogress', (file) => {
-    //   assetText.setText(`Loading asset: ${file.key}`);
-    // });
-
-
-    // this.load.on('complete', () => {
-    //   progressBar.destroy();
-    //   progressBox.destroy();
-    //   loadingText.destroy();
-    //   percentText.destroy();
-    //   assetText.destroy();
-    //   this.ready();
-    // });
-
-    // this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
-
-
-    // this.load.image('blueButton1', blueButton1);
-    // this.load.image('blueButton2', blueButton2);
-    // this.load.image('phaserLogo', phaserLogo);
-    //
-    // this.load.image('box', box);
-    // this.load.image('checkedBox', checkedBox);
-    // this.load.audio('bgMusic', [bgMusic]);
+    this.load.image('box', box);
+    this.load.image('checkedBox', checkedBox);
+    this.load.audio('bgMusic', [bgMusic]);
   }
 
-  // ready() {
-  //   this.scene.start('Title');
-  //   this.readyCount += 1;
-  //   if (this.readyCount === 2) {
-  //     this.scene.start('Title');
-  //   }
-  // }
+  ready() {
+    this.scene.start('Title');
+    this.readyCount += 1;
+    if (this.readyCount === 2) {
+      this.scene.start('Title');
+    }
+  }
 }
